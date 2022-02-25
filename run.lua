7254f21bffc888ac7b368ac5e84c8069c4b80341
igorkll <69525332+igorkll@users.noreply.github.com>
2022-02-25 13:16:11 +0300

    Add files via upload

---
+++ run.lua
@@ -0,0 +1,38 @@
+local fs = require("filesystem")
+local su = require("superUtiles")
+local shell = require("shell")
+local serialization = require("serialization")
+
+-----------------------------------
+
+local args = shell.parse(...)
+local arg = {table.unpack(args, 2, #args)}
+
+-----------------------------------
+
+if args[1] then
+    local code = assert(loadfile(shell.resolve(args[1])))
+
+    os.setenv("_", args[1])
+    local out = {xpcall(code, debug.traceback, table.unpack(arg))}
+    for i = 1, #out do
+        if type(out[i]) == "table" then
+            local ok, value = pcall(serialization.serialize, out[i])
+            if ok then
+                out[i] = value
+            else
+                out[i] = tostring(out[i])
+            end
+        else
+            out[i] = tostring(out[i])
+        end
+    end
+    out = table.concat(out, "\n")
+
+    local filename = os.tmpname()
+    su.saveFile(filename, out)
+    shell.execute("view", _ENV, filename)
+    shell.execute("rm", _ENV, filename)
+else
+    print("path ...")
+end
